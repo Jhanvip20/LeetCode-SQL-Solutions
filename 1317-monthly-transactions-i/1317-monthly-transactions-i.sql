3b85cd178eb1d567ea1ab748254b61df11f25491
Jhanvi Patel <117141234+Jhanvip20@users.noreply.github.com>
2025-03-04 18:29:07 -0500

    Time: 1110 ms (47.15%), Space: 0B (100%) - LeetHub

--- 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
@@ -3,6 +3,7 @@ SELECT
     DATE_FORMAT(trans_date, '%Y-%m') AS month,
     country,
     COUNT(*) AS trans_count,
+    # COALESCE() ensures NULL values are replaced with 0.
     COALESCE(SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END), 0) AS   approved_count,
     SUM(amount) AS trans_total_amount,
     COALESCE(SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END), 0) AS approved_total_amount
