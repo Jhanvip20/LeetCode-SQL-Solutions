690bab3c322a9ec14d5be4d0f42965b72cc5be7d
Jhanvi Patel <117141234+Jhanvip20@users.noreply.github.com>
2025-03-05 18:51:12 -0500

    Time: 763 ms (86.41%), Space: 0B (100%) - LeetHub

--- 1842-number-of-calls-between-two-persons/1842-number-of-calls-between-two-persons.sql
+++ 1842-number-of-calls-between-two-persons/1842-number-of-calls-between-two-persons.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
 SELECT 
-    LEAST (from_id, to_id) AS person1,
-    GREATEST (from_id, to_id) AS person2,
+    LEAST (from_id, to_id) AS person1,  # LEAST(value1, value2, ...): Returns the smallest value from the given list.
+    GREATEST (from_id, to_id) AS person2, # GREATEST(value1, value2, ...): Returns the largest value from the given list.
     COUNT(*) AS call_count,
     SUM(duration) AS total_duration
 FROM Calls
