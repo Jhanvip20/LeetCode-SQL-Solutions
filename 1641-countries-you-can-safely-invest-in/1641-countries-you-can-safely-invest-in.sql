7b830a9713a26fa86c02c8617eaafd2943d3ff63
Jhanvi Patel <117141234+Jhanvip20@users.noreply.github.com>
2025-03-05 23:54:58 -0500

    Time: 3095 ms (9.53%), Space: 0B (100%) - LeetHub

--- 1641-countries-you-can-safely-invest-in/1641-countries-you-can-safely-invest-in.sql
+++ 1641-countries-you-can-safely-invest-in/1641-countries-you-can-safely-invest-in.sql
@@ -5,6 +5,6 @@ from(
         ,avg(ca.duration) over(partition by c.name) avg_duration
         ,avg(ca.duration) over() avg_total
     from calls ca left join person p on (ca.caller_id = p.id) or (ca.callee_id = p.id)
-    inner join country c on substring(p.phone_number,1,3) = c.country_code
+    inner join country c on substring(p.phone_number,1,3) = c.country_code #The SUBSTRING(p.phone_number,1,3) assumes a fixed-length country code, but some country codes are not always 3 digits.
 ) a
 where avg_duration > avg_total
