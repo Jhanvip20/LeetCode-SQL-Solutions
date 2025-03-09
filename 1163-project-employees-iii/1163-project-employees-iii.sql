7a81e77691e567f61f3bb02e84abd5b01c172d68
Jhanvi Patel <117141234+Jhanvip20@users.noreply.github.com>
2025-03-08 22:47:49 -0500

    Time: 1989 ms (5.01%), Space: 0B (100%) - LeetHub

--- 1163-project-employees-iii/1163-project-employees-iii.sql
+++ 1163-project-employees-iii/1163-project-employees-iii.sql
@@ -2,9 +2,10 @@
 SELECT p.project_id, p.employee_id
 FROM Project p
 JOIN Employee e ON p.employee_id = e.employee_id
-JOIN (
-    SELECT project_id, MAX(experience_years) AS max_experience
-    FROM Project p
-    JOIN Employee e ON p.employee_id = e.employee_id
-    GROUP BY project_id
-) max_exp ON p.project_id = max_exp.project_id AND e.experience_years = max_exp.max_experience;
+WHERE e.experience_years = (
+    SELECT MAX(experience_years)
+    FROM Employee
+    WHERE employee_id IN (
+        SELECT employee_id FROM Project WHERE project_id = p.project_id
+    )
+);
