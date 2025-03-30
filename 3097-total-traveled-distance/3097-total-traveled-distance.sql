1c57bf0ec65b20cbf2257ed2b87c0d3db424c5f6
Jhanvi Patel <117141234+Jhanvip20@users.noreply.github.com>
2025-03-30 08:15:50 -0400

    Time: 865 ms (69.42%), Space: 0B (100%) - LeetHub

---
+++ 3097-total-traveled-distance/3097-total-traveled-distance.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT  user_id,
+        name,
+        IFNULL(
+            SUM(distance),
+            0
+        ) "traveled distance"
+FROM Users
+    LEFT JOIN Rides
+    USING (user_id)
+GROUP BY 1
+ORDER BY 1;
